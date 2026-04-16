6e518849559c1ba2f8ec0a568e035218718b5e60
amyshaqistina <amyshaqistina03@gmail.com>
2026-04-16 20:24:25 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect(url('/admin'));
-    // return view('welcome');
 });
 
 
 Route::get('/change-password', ChangePassword::class);
+
 
 
