28c04c7de56dba2d212df6dda6bca96735f07051
NabilahNordin <nabilahahmad.nordin@live.iium.edu.my>
2026-04-23 11:51:43 +0800

    update student pages

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -72,8 +72,8 @@
             <div class="bg-white rounded-2xl shadow-sm p-8 mt-5">
 
 
-               
-                   
+
+
                         <div class="flex items-start gap-4">
                             <svg class="w-6 h-6 text-gray-700 mt-1 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
@@ -93,6 +93,7 @@
                                     <li class="flex items-start gap-3">
                                         <span class="text-blue-600 mt-1">•</span>
                                         <span>Crisis reports are handled with strict confidentiality</span>
+                                        <span>bhahhahhahahha yekyekyek</span>
                                     </li>
                                     <li class="flex items-start gap-3">
                                         <span class="text-blue-600 mt-1">•</span>
@@ -105,8 +106,8 @@
                                 </ul>
                             </div>
                         </div>
-                  
-             
+
+
 
 
             </div>
