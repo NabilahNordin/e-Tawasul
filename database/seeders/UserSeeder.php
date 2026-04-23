bef447a9f1920340a1241b37b4f12148f5ca712c
Nabilah <luqman.nordin@LPPSA.GOV>
2026-04-23 10:43:19 +0800

    update3

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
@@ -14,6 +16,12 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
+        Schema::disableForeignKeyConstraints();
+        DB::table('users')->truncate();
+        Schema::enableForeignKeyConstraints();
+
+
         // Create a student user
         $user = User::create([
             'name' => 'Student ',
