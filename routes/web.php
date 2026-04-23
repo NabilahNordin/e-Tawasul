de8d31ab22309c26fa4444518da5b8f5918d14cb
Nabilah <luqman.nordin@LPPSA.GOV>
2026-04-23 11:53:20 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return redirect(url('/admin'));
-    // return view('welcome');
+    // return redirect(url('/admin'));
+    return view('welcome');
 });
 
 
