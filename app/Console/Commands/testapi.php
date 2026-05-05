ee8718eb548b1148dd36b4ab85a77e32dd9c2cee
NabilahNordin <nabilahahmad.nordin@live.iium.edu.my>
2026-05-05 21:53:00 +0800

    Update to connect

--- app/Console/Commands/testapi.php
+++ app/Console/Commands/testapi.php
@@ -42,11 +42,11 @@ class testapi extends Command
 >>>>>>> b2ead1f1c58bbdde8542f289a10a0cc47fa9911a
         //$getSchedule = $this->getSchedule($data);
 
-        // $getListStaff = $this->getListStaff();  
+        // $getListStaff = $this->getListStaff();
 
 
         // dd($getSchedule);
-        dump($getProfile);
+        dd($getProfile);
         // dd($getListStaff);
 
 
